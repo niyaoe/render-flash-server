aa2072fcbf8d91ecd21236c03508fa5132bcc55c
niyaoe <niyas4334@gmail.com>
2026-03-17 13:54:45 +0530

    cors

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const connection = require("./MogoDB/Config")
 const dotenv = require("dotenv");
 const app = express();
+const cors = require("cors")
 
 dotenv.config();
 
 connection();
+app.use(cors({
+    origin:"http://localhost:5173"
+}))
 
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5002
 app.listen(PORT,()=>{console.log(`server is ok on ${PORT}`);
 })
