0285aca3efcca0cc985c5a70e656e1f5ff2dbe68
niyaoe <niyas4334@gmail.com>
2026-06-03 23:22:47 +0530

    usersection backend strted controllers

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -35,3 +35,44 @@ exports.updateProfile = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+//get all user
+
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find()
+      .select("-password")
+      .sort({ createdAt: -1 });
+
+    res.json(users);
+
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+//get single user
+
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.json(user);
+
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,13 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const protect = require("../middleware/auth.middleware");
+
 const {
   getMe,
   updateProfile,
+  getAllUsers,
+  getUserById,
 } = require("../controllers/user.controller");
 
-/* protected routes */
+/* CURRENT USER */
 router.get("/me", protect, getMe);
 router.put("/update", protect, updateProfile);
+
+/* USERS */
+router.get("/all", protect, getAllUsers);
+router.get("/:id", protect, getUserById);
 
 module.exports = router;
