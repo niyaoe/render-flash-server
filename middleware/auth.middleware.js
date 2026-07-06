13cd2943625124bff4b0e941b1dea26e36597658
niyaoe <niya4334@gmail.com>
2026-07-06 15:31:33 +0530

    debugged for umair

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -5,6 +5,8 @@ const protect = async (req, res, next) => {
   let token;
 
   try {
+    // console.log("req.headers.authorization : ",req.headers.authorization);
+    
     // get token from header
     if (
       req.headers.authorization &&
@@ -13,9 +15,13 @@ const protect = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // console.log("decoded :",decoded);
+      
 
       // attach user to request (without password)
       req.user = await User.findById(decoded.id).select("-password");
+      // console.log("req.user : ",req.user);
+      
 
       next();
     } else {
