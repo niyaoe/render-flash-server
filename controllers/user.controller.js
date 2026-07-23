ea69e93415d28e8dd3fb9273b7ca625a1dcbf9fd
niyaoe <niya4334@gmail.com>
2026-07-23 17:38:31 +0530

    pagination on users on both

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -66,12 +66,46 @@ exports.updateProfile = async (req, res) => {
 
 exports.getAllUsers = async (req, res) => {
   try {
-    const users = await User.find()
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 20;
+    const search = req.query.search || "";
+
+    const skip = (page - 1) * limit;
+
+    const filter = search
+      ? {
+          $or: [
+            {
+              username: {
+                $regex: search,
+                $options: "i",
+              },
+            },
+            {
+              name: {
+                $regex: search,
+                $options: "i",
+              },
+            },
+          ],
+        }
+      : {};
+
+    const totalUsers = await User.countDocuments(filter);
+
+    const users = await User.find(filter)
       .select("-password")
-      .sort({ createdAt: -1 });
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
 
-    res.json(users);
-
+    res.json({
+      users,
+      currentPage: page,
+      totalPages: Math.ceil(totalUsers / limit),
+      totalUsers,
+      hasMore: skip + users.length < totalUsers,
+    });
   } catch (err) {
     res.status(500).json({
       message: err.message,
